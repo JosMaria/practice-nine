f4b4a3e7c4226a22381cd00aa620d4ae62d9b7dc
Jose Maria <josmariaguilar@gmail.com>
2022-06-19 18:50:52 -0400

    Exercise three implemented

--- src/main/java/org/genesiscode/practicenine/view/ExerciseThreePane.java
+++ src/main/java/org/genesiscode/practicenine/view/ExerciseThreePane.java
@@ -1,7 +1,11 @@
 package org.genesiscode.practicenine.view;
 
 import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.control.Label;
 import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import org.genesiscode.practicenine.service.ExerciseThree;
 import org.genesiscode.practicenine.service.Util;
@@ -14,6 +18,8 @@ public class ExerciseThreePane extends MyPane {
     private static ExerciseThreePane exerciseThreePane;
     private final ExerciseThree exerciseThree;
     private TableView<RowResultThree> resultTable;
+    private TextField fieldExponentialMedia, fieldWarehouseCapacity,
+            fieldCostOfOrdering, fieldShortageCost, fieldMaintenanceCost;
 
     private ExerciseThreePane() {
         super("EJERCICIO 3");
@@ -24,13 +30,25 @@ public class ExerciseThreePane extends MyPane {
 
     @Override
     protected void click_btn_start() {
-        exerciseThree.setRandomNumbers(Util.convertToList(areaRandomNumbers.getText()));
+        try {
+            int exponentialMedia = Integer.parseInt(fieldExponentialMedia.getText());
+            int warehouseCapacity = Integer.parseInt(fieldWarehouseCapacity.getText());
+            int costOfOrdering = Integer.parseInt(fieldCostOfOrdering.getText());
+            int shortageCost = Integer.parseInt(fieldShortageCost.getText());
+            int maintenanceCost = Integer.parseInt(fieldShortageCost.getText());
 
-        // input data
-        int exponentialMedia = 100, warehouseCapacity = 700, costOfOrdering = 1000, shortageCost = 6, maintenanceCost = 1;
-        resultTable.setItems(exerciseThree.getResultList
-                (exponentialMedia, warehouseCapacity, costOfOrdering, shortageCost, maintenanceCost));
-        AssistPane.show(resultTable);
+            exerciseThree.setRandomNumbers(Util.convertToList(areaRandomNumbers.getText()));
+            resultTable.setItems(exerciseThree.getResultList
+                    (exponentialMedia, warehouseCapacity, costOfOrdering, shortageCost, maintenanceCost));
+            AssistPane.show(resultTable);
+        } catch (Exception e) {
+            String message = """
+                    Los datos de entrada
+                    no cumplenel formato
+                        implementado
+                    """;
+            MessageBox.show(message);
+        }
     }
 
     public synchronized static ExerciseThreePane getInstance() {
@@ -38,6 +56,17 @@ public class ExerciseThreePane extends MyPane {
     }
 
     private void loadControls() {
+        int columnCount = 5;
+        fieldExponentialMedia = new TextField();
+        fieldExponentialMedia.setPrefColumnCount(columnCount);
+        fieldWarehouseCapacity = new TextField();
+        fieldWarehouseCapacity.setPrefColumnCount(columnCount);
+        fieldCostOfOrdering = new TextField();
+        fieldCostOfOrdering.setPrefColumnCount(columnCount);
+        fieldShortageCost = new TextField();
+        fieldShortageCost.setPrefColumnCount(columnCount);
+        fieldMaintenanceCost = new TextField();
+        fieldMaintenanceCost.setPrefColumnCount(columnCount);
         buildResultTablePane();
     }
 
@@ -59,8 +88,24 @@ public class ExerciseThreePane extends MyPane {
                         column("Costo\nTotal", "totalCost", 90)));
     }
 
+    private VBox buildInputPane() {
+        HBox paneOne = new HBox(10, new Label("Exponecial Media"), fieldExponentialMedia);
+        paneOne.setAlignment(Pos.CENTER_RIGHT);
+        HBox paneTwo = new HBox(10, new Label("Capacidad de Bodega"), fieldWarehouseCapacity);
+        paneTwo.setAlignment(Pos.CENTER_RIGHT);
+        HBox paneThree = new HBox(10, new Label("Costo de ordenar"), fieldCostOfOrdering);
+        paneThree.setAlignment(Pos.CENTER_RIGHT);
+        HBox paneFour = new HBox(10, new Label("Costo de faltante"), fieldShortageCost);
+        paneFour.setAlignment(Pos.CENTER_RIGHT);
+        HBox paneFive = new HBox(10, new Label("Costo de mantenimiento"), fieldMaintenanceCost);
+        paneFive.setAlignment(Pos.CENTER_RIGHT);
+        return new VBox(10, paneOne, paneTwo, paneThree, paneFour, paneFive);
+    }
+
     private void buildPane() {
-        mainPane = new VBox(10, title, inputPane, btnStart);
-        mainPane.setPadding(new Insets(20));
+        VBox rightPane = new VBox(10, inputPane, btnStart);
+        mainPane = new VBox(10, title, new HBox(30, rightPane, buildInputPane()));
+        mainPane.setPadding(new Insets(10));
+        mainPane.setAlignment(Pos.CENTER);
     }
 }
